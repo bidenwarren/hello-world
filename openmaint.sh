0ba2029f2b3461a63e9956de161dae1b6000f0b6
bidenwarren <55264681+bidenwarren@users.noreply.github.com>
2020-05-11 01:02:43 +0000

    Create openmaint.sh

---
+++ openmaint.sh
@@ -0,0 +1,9 @@
+sudo wget https://downloads.bitnami.com/files/stacks/alfresco/201911-7/bitnami-alfresco-201911-7-linux-x64-installer.run
+sudo mv bitnami-alfresco-201911-7-linux-x64-installer.run openmaint.run
+sudo chmod +x openmaint.run
+
+sudo wget https://svwh.dl.sourceforge.net/project/openmaint/2.0/openmaint-2.0-3.1a.war
+sudo wget https://phoenixnap.dl.sourceforge.net/project/openmaint/2.0/openmaint-2.0-3.1a.sh
+sudo wget https://svwh.dl.sourceforge.net/project/openmaint/2.0/openmaint-2.0-3.1a-resources.zip
+sudo wget https://svwh.dl.sourceforge.net/project/openmaint/2.0/openmaint-2.0-3.1a-resources.tar.gz
+
